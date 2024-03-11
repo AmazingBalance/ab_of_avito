b92615f53527e98086898fc68f1139d20edb585d
Konstantin Borisenko <kostyaborisenko05@gmail.com>
2024-03-11 22:56:08 +0300

    Nuked routes. Rewriting routs

--- core.go
+++ core.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -17,45 +16,20 @@ func NewRoute(pattern string, handler func(http.ResponseWriter, *http.Request)) 
 
 func Router() {
 	NewRoute("/", Welcome) // Inacceseble from Frontend
+}
 
-	NewRoute("/api/edit", HandleEdit)
+func illegal(w http.ResponseWriter) {
+	http.Error(w, "That's illegal, Sorry can't do that.", http.StatusMethodNotAllowed)
 }
 
 func Welcome(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Hello there sysadmin.")
 }
 
-func HandleEdit(w http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case http.MethodGet:
-		RespondEdit(w, r)
-	case http.MethodPost:
-		CatchEdit(w, r)
-	default:
-		http.Error(w, "That's illegal, Sorry can't do that.", http.StatusMethodNotAllowed)
-	}
-}
-
-func RespondEdit(w http.ResponseWriter, r *http.Request) {
 	// For communication
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
-	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
-	fmt.Fprintf(w, "Hello there.")
-	log.Println("Responded to edit request.")
-}
-
-func CatchEdit(w http.ResponseWriter, r *http.Request) {
-	log.Println("Catching stuff.")
-	var stuff string
-
-	if err := json.NewDecoder(r.Body).Decode(&stuff); err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-
-}
+	// w.Header().Set("Access-Control-Allow-Origin", "*")
+	// w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	// w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 
 func main() {
 	Router()

dd04713a9c195373cff6aca823d13b2b0e1d7960
Konstantin Borisenko <kostyaborisenko05@gmail.com>
2024-03-11 15:17:10 +0300

    Better reset function. Fetching stuff now

--- dummy_ddos.js
+++ dummy_ddos.js
@@ -1,16 +1,22 @@
 console.log("Hello there. My name is JS.")
 
 var b1 = -1
+var b2 = -1
+
+const rootURI = "http://localhost:6969"
 
 function Reset() {
-    for (i = 1; i < 10; i++) {
-        const el = document.getElementById(`fbtn${i}`)
-        if (el != null) {
-            console.log(`Deleted inner stuff in div#${el.id}`)
-            el.innerHTML = ""
-            b1 = 0
-        }
+
+    const roots = document.getElementsByClassName("root")
+    var i = 0
+    while (i < roots.length) {
+        roots[i].innerHTML = ""
+
+        i++
     }
+
+    b1 = -1
+    b2 = -1
 }
 
 function Btn1() {
@@ -26,4 +32,35 @@ function Btn1() {
     nw.innerText = `Hello there. I am no.${b1}!`
 
     el.appendChild(nw)
+}
+
+function Broot() {
+    const el = document.getElementById("fbroot")
+    const ne = document.createElement("p")
+
+    if (b2 % 5 == 4) {
+        el.innerHTML = ""
+    }
+
+    fetch(rootURI + "/api/edit").then((response) => {
+        if (!response.ok) {
+            const err = `HTTP error: ${response.status}`
+            ne.innerText = err
+            el.appendChild(ne)
+            throw new Error(err)
+        }
+        return response.text
+    }).then((text) => {
+        console.log(`Got: ${text}`)
+
+        ne.innerText = text
+        el.appendChild(ne)
+    }).catch((response) => {
+        console.log(response)
+
+        ne.innerText = response
+        el.appendChild(ne)
+    })
+
+    b2++
 }

cdb9def66538baef476c10808bbd59f460f17219
Konstantin Borisenko <kostyaborisenko05@gmail.com>
2024-03-11 15:18:18 +0300

    Added new route. Better route addition with logs.

--- core.go
+++ core.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -8,16 +9,56 @@ import (
 
 const PORT string = ":6969"
 
-func router() {
-	http.HandleFunc("/", welcome) // Inacceseble from Frontend
+func NewRoute(pattern string, handler func(http.ResponseWriter, *http.Request)) {
+	log.Println("Adding new route " + pattern)
+
+	http.HandleFunc(pattern, handler)
 }
 
-func welcome(w http.ResponseWriter, _ *http.Request) {
+func Router() {
+	NewRoute("/", Welcome) // Inacceseble from Frontend
+
+	NewRoute("/api/edit", HandleEdit)
+}
+
+func Welcome(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w, "Hello there sysadmin.")
 }
 
+func HandleEdit(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		RespondEdit(w, r)
+	case http.MethodPost:
+		CatchEdit(w, r)
+	default:
+		http.Error(w, "That's illegal, Sorry can't do that.", http.StatusMethodNotAllowed)
+	}
+}
+
+func RespondEdit(w http.ResponseWriter, r *http.Request) {
+	// For communication
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	fmt.Fprintf(w, "Hello there.")
+	log.Println("Responded to edit request.")
+}
+
+func CatchEdit(w http.ResponseWriter, r *http.Request) {
+	log.Println("Catching stuff.")
+	var stuff string
+
+	if err := json.NewDecoder(r.Body).Decode(&stuff); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+}
+
 func main() {
-	router()
+	Router()
 
 	log.Println("Starting server...")
 	err := http.ListenAndServe(PORT, nil)

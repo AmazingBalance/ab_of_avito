5409496fdbcd4dd18e4d7f19dfe9619c5ca82585
NikDimer <60222621+NikDimer@users.noreply.github.com>
2024-03-13 01:39:59 +0300

    commit #3

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,28 +1,76 @@
 import logo from "./images/logo48.png";
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import ActiveMatrixItem from "./components/ActiveMatrixItem/ActiveMatrixItem";
+import React, { useEffect, useState, useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
+
 
 function App() {
-    var ActiveMatrixList = [];
-    const [activeIndex, setActiveIndex] = useState(-1);
+    const [, forceUpdate] = useReducer(x => x + 1, 0);
+    //Opened Matrix States
+    const [openedMatrixList, setOpenedMatrixList] = useState([
+        { name: "baseline_matrix_1.sql", id: uuidv4(), type: "baseline", active: false },
+        { name: "baseline_matrix_2.sql", id: uuidv4(), type: "baseline", active: false },
+        { name: "baseline_matrix_3.sql", id: uuidv4(), type: "baseline", active: false },
+        { name: "discount_matrix_1.sql", id: uuidv4(), type: "discount", active: false },
+        { name: "discount_matrix_2.sql", id: uuidv4(), type: "discount", active: false },
+        { name: "discount_matrix_3.sql", id: uuidv4(), type: "discount", active: false },
+    ]);
 
-    function AddActiveMatrix(name) {
-        ActiveMatrixList.push({ name: name, index: ActiveMatrixList.length });
-        if (activeIndex == -1) {
-            setActiveIndex(0);
+    //Active Matrix States
+    const [activeIndex, setActiveIndex] = useState(-1);
+    const [activeMatrixList, setActiveMatrixList] = useState([]);
+    var changeActive = () => { };
+
+    //Active Matrix Functions
+    function AddActiveMatrix(name, id) {
+        setActiveMatrixList([...activeMatrixList, { name: name, id: id }]);
+
+        if (activeIndex === -1) {
+            setActiveIndex(id);
         }
     }
 
-    function handleClick(ind) {
-        console.log(ind);
+    function HandleActiveClick(ind) {
         setActiveIndex(ind);
     }
 
-    //useEffect(() => {}, []);
+    function HandleActiveDeleteClick(ind) {
+        for (let i = 0; i < openedMatrixList.length; i++) {
+            console.log(openedMatrixList[i], ind)
+            if (openedMatrixList[i].id === ind) {
+                openedMatrixList[i].active = false;
+                break;
+            }
+        }
+        if (activeMatrixList.length === 1) {
+            setActiveIndex(-1);
+            setActiveMatrixList([]);
+        } else {
+            if (activeMatrixList[0].id === ind) {
+                setActiveIndex(activeMatrixList[1].id)
+                activeMatrixList.splice(0, 1);
+            } else {
+                let t = -1;
+                for (let i = 0; i < activeMatrixList.length; i++) {
+                    if (activeMatrixList[i].id === ind) {
+                        activeMatrixList.splice(i, 1);
+                        break;
+                    }
+                    t = activeMatrixList[i].id;
+                }
+                setActiveIndex(t);
+            }
+        }
+        forceUpdate()
+    }
+    //Opened Matrix Functions
 
-    AddActiveMatrix("baseline_matrix_1.sql");
-    AddActiveMatrix("baseline_matrix_2.sql");
+    function ActivateOpenedMatrix(name, id, n) {
+        if (!openedMatrixList[n].active) {
+            AddActiveMatrix(name, id);
+            openedMatrixList[n].active = true;
+        }
+    }
 
     return (
         <div className="App">
@@ -40,40 +88,38 @@ function App() {
                 <div className="OpenedMatrix">
                     <h2 className="OpenedMatrixTitle">Основные матрицы</h2>
                     <ul className="OpenedMatrixListMain">
-                        <li className="OpenedMatrixList_Item">
-                            <p>baseline_matrix_1.sql</p>
-                        </li>
-                        <li className="OpenedMatrixList_Item">
-                            <p>baseline_matrix_2.sql</p>
-                        </li>
-                        <li className="OpenedMatrixList_Item">
-                            <p>baseline_matrix_3.sql</p>
-                        </li>
+                        {openedMatrixList.map((elem) => elem.type === "baseline" ?
+                            <li className="OpenedMatrixList_Item" key={elem.id.toString()} onClick={() => ActivateOpenedMatrix(elem.name, elem.id, openedMatrixList.indexOf(elem))}>
+                                <p>{elem.name}</p>
+                            </li> : <></>
+                        )}
                     </ul>
 
                     <h2 className="OpenedMatrixTitle">Скидочные матрицы</h2>
                     <ul className="OpenedMatrixListSpecial">
-                        <li className="OpenedMatrixList_Item">
-                            <p>discount_matrix_1.sql</p>
-                        </li>
-                        <li className="OpenedMatrixList_Item">
-                            <p>discount_matrix_2.sql</p>
-                        </li>
-                        <li className="OpenedMatrixList_Item">
-                            <p>discount_matrix_3.sql</p>
-                        </li>
+                        {openedMatrixList.map((elem) => elem.type === "discount" ?
+                            <li className="OpenedMatrixList_Item" key={elem.id.toString()} onClick={() => ActivateOpenedMatrix(elem.name, elem.id, openedMatrixList.indexOf(elem))}>
+                                <p>{elem.name}</p>
+                            </li> : <></>
+                        )}
                     </ul>
                 </div>
                 <div className="WorkZone">
                     <header className="WorkZoneHeader">
                         <ul className="ActiveMatrixList">
-                            {ActiveMatrixList.map((elem) => (
-                                <ActiveMatrixItem
-                                    name={elem.name}
-                                    index={elem.index}
-                                    key={elem.index}
-                                    onClick={() => handleClick(elem.index)}
-                                />
+                            {activeMatrixList.map((elem) => (
+                                <li
+                                    className="ActiveMatrixList_Item"
+                                    style={elem.id === activeIndex ? { "backgroundColor": "#515151" } : {}}
+                                    key={elem.id.toString()}
+                                >
+                                    <div onClick={() => HandleActiveClick(elem.id)}>
+                                        <p>{elem.name}</p>
+                                    </div>
+                                    <div onClick={() => HandleActiveDeleteClick(elem.id)}>
+                                        <span>✕</span>
+                                    </div>
+                                </li>
                             ))}
                         </ul>
                     </header>

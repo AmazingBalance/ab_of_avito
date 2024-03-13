fde8c72a056f5f5d548ee81411f86431b7ef3cca
Konstantin Borisenko <kostyaborisenko05@gmail.com>
2024-03-13 23:52:40 +0300

    Added some NotYetAPI API

--- core.go
+++ core.go
@@ -29,10 +29,62 @@ func Router() {
 	// Testing routes. If you connect to server directly
 	NewRoute("/hello", tosting.Welcome) // Inacceseble from Frontend side
 	NewRoute("/json", tosting.Testing)  // Testing JSON communication
-
 	NewRoute("/api/getMatrixNOOO", getMatrixNOOO)
 
+	NewRoute("/NotYetDone", NotYetDone)
+
 	NewRoute("/api/getCategories", getCats)
+
+	NewRoute("/api/getLocations", NotYetDone)
+	NewRoute("/api/getPrice", NotYetDone)
+	NewRoute("/api/getData", NotYetDone)
+	NewRoute("/api/createDatabase", NotYetDone)
+	NewRoute("/api/createStorage", NotYetDone)
+	NewRoute("/api/editDatabase", NotYetDone)
+	NewRoute("/api/getStorage", NotYetDone)
+}
+
+func NotYetDone(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
+	switch r.Method {
+	case http.MethodPost:
+		defer r.Body.Close()
+
+		var tmp struct {
+			Status string `json:"status"`
+		}
+		tmp.Status = "NotYetDone"
+
+		rb, err := json.Marshal(tmp)
+		if err != nil {
+			log.Fatal("Failing to make response.")
+			http.Error(w, "Something went wrong packing JSON.", http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(rb)
+	case http.MethodGet:
+		defer r.Body.Close()
+
+		var tmp struct {
+			Status string `json:"status"`
+		}
+		tmp.Status = "NotYetDone"
+
+		rb, err := json.Marshal(tmp)
+		if err != nil {
+			log.Fatal("Failing to make response.")
+			http.Error(w, "Something went wrong packing JSON.", http.StatusInternalServerError)
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(rb)
+	default:
+		http.Error(w, "No no No no", http.StatusMethodNotAllowed)
+	}
 }
 
 func getCats(w http.ResponseWriter, r *http.Request) {
@@ -91,7 +143,7 @@ func getMatrixNOOO(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func mainy() {
+func main() {
 	Router()
 
 	log.Println("Starting server...")
@@ -101,10 +153,4 @@ func mainy() {
 		log.Fatal("Server is unable to start.")
 		log.Fatal(err.Error())
 	}
-}
-
-func main() {
-	row, err := modules.Selector("./data/baseline_matrix_1.db", 1, 1)
-	fmt.Println(err)
-	fmt.Println(row)
 }
